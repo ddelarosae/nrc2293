28f8dd7f6e9dd163c7f961aa9a40a07fd70fa232
ddelarosae <ddelarosae@uninorte.edu.co>
2022-11-28 08:26:55 -0500

    firebase auth email/password

--- s10firebase_android/lib/main.dart
+++ s10firebase_android/lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:s10firebase_android/page/user_page.dart';
-import 'package:s10firebase_android/model/user.dart';
+import 'package:s10firebase_android/page/home_page.dart';
+import 'package:s10firebase_android/widget/login_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 class MyApp extends StatelessWidget {
-  static final String title = "FireStore Read and Write NRC2293";
+  static const String title = "FireStore Auth Email NRC2293";
+
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) => MaterialApp(
+        navigatorKey: navigatorKey,
+        debugShowCheckedModeBanner: false,
         title: title,
         theme: ThemeData(
           primarySwatch: Colors.green,
@@ -29,74 +35,24 @@ class MyApp extends StatelessWidget {
       );
 }
 
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   const MainPage({super.key});
-
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  final controller = TextEditingController();
-
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Lista de usuarios'),
-        ),
-        body: buildUsers(),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => UserPage(),
-            ));
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                  child: Text('Existe el siguiente error: ${snapshot.error}'));
+            } else if (snapshot.hasData) {
+              return const HomePage();
+            } else {
+              return const LoginWidget();
+            }
           },
         ),
       );
-
-  Widget buildUsers() => StreamBuilder<List<User>>(
-      stream: readUsers(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text('Ha ocurrido este error ${snapshot.error}');
-        } else if (snapshot.hasData) {
-          final users = snapshot.data!;
-          return ListView(
-            children: users.map(buildUser).toList(),
-          );
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      });
-  Widget buildUser(User user) => ListTile(
-        leading: CircleAvatar(child: Text('${user.age}')),
-        title: Text(user.name),
-        subtitle: Text(user.birthday.toIso8601String()),
-      );
-
-  Stream<List<User>> readUsers() => FirebaseFirestore.instance
-      .collection('users')
-      .snapshots()
-      .map((snapshot) =>
-          snapshot.docs.map((doc) => User.fromJson(doc.data())).toList());
-
-  Future<User?> readUser() async {
-    final docUser = FirebaseFirestore.instance.collection('users').doc('my-id');
-    final snapshot = await docUser.get();
-    if (snapshot.exists) {
-      return User.fromJson(snapshot.data()!);
-    }
-  }
-
-  Future createUser({required String name}) async {
-    final docUser = FirebaseFirestore.instance.collection('users').doc();
-    final json = {
-      'id': docUser.id,
-      'name': name,
-      'age': 21,
-      'birthday': DateTime(2022, 11, 25),
-    };
-    await docUser.set(json);
-  }
 }
