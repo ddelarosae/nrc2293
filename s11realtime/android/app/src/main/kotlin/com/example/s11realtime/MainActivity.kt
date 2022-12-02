06f02602ca2baef97ab8da8b376488022d3c5d05
ddelarosae <ddelarosae@uninorte.edu.co>
2022-12-02 12:25:51 -0500

    ajustes firebase

---
+++ s11realtime/android/app/src/main/kotlin/com/example/s11realtime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.s11realtime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ s11realtime/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:s11realtime/page/MessagePage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: MessagePage(),
+    );
+  }
+
+}
